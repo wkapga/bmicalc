822d9b10eb789860a2c1f1d5136bee268158d972
Gabriel Panzenboeck <gabriel.panzenboeck@gmail.com>
2011-09-19 14:50:33 +0200

    several changes, import of percentiles-data

--- src/com/example/bmicalc/BmicalcActivity.java
+++ src/com/example/bmicalc/BmicalcActivity.java
@@ -5,10 +5,9 @@ package com.example.bmicalc;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-// import JSci.maths.statistics;
-
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,9 +22,13 @@ public class BmicalcActivity extends Activity implements OnClickListener {
 	protected Button CalcLabel;
 	protected TextView ErgebnisLabel;
 	protected EditText HeightLabel;
-	protected EditText WeightLabel;
+	protected EditText WeightLabel;	
+	protected EditText AgeLabel;
 	
-	
+	protected double[] avalues;
+	protected double[] lvalues;
+	protected double[] svalues;
+	protected double[] pvalues;
 	
 	/** Called when the activity is first created. */
     @Override
@@ -38,12 +41,48 @@ public class BmicalcActivity extends Activity implements OnClickListener {
         ErgebnisLabel = (TextView) findViewById(R.id.et_result);	
         HeightLabel = (EditText) findViewById(R.id.et_height);
     	WeightLabel = (EditText) findViewById(R.id.et_weight);
-        
+    	AgeLabel = (EditText) findViewById(R.id.et_age);
+            
         
         CalcLabel.setOnClickListener( this);
         
-        HeightLabel.setText("180");
-        WeightLabel.setText("80");
+        
+        
+        Resources res = getResources();
+        String[] perc = res.getStringArray(R.array.boys);
+        
+        int n = perc.length;
+        
+        Double[] avalues = new Double[n];
+        Double[] lvalues = new Double[n]; 
+        Double[] svalues = new Double[n]; 
+        Double[] pvalues = new Double[n]; 
+        
+        
+        for (int i=0; i < n; i++) 
+        {
+        	
+        	String x1 = (perc[i]).toString();
+        	String x[] = x1.split(" ");
+        	avalues[i] = Double.parseDouble( x[0].toString() );
+        	lvalues[i] = Double.parseDouble( x[1].toString() ); 
+        	svalues[i] = Double.parseDouble( x[2].toString() ); 
+        	pvalues[i] = Double.parseDouble( x[6].toString() ); 
+            
+        }
+        	
+        
+       // NumberFormat formatter = new DecimalFormat(".00");
+       // String s2 =  formatter.format(lvalues[8]);
+        String s2 =  Double.toString(avalues[8])+Double.toString(pvalues[8])+Double.toString(lvalues[8])+Double.toString(svalues[8]);
+
+        ErgebnisLabel.setText(s2);
+        
+        
+        
+        HeightLabel.setText("105");
+        WeightLabel.setText("14.5");
+        AgeLabel.setText("4.5");
     }
         
     public void onClick(View v) {
@@ -67,7 +106,7 @@ public class BmicalcActivity extends Activity implements OnClickListener {
                  NumberFormat formatter = new DecimalFormat(".00");
                  String s3 = formatter.format(bmi); 
                  
-            	ErgebnisLabel.setText(  getString(R.string.result1)  + s3 + getString(R.string.result2));
+            	ErgebnisLabel.setText(  getString(R.string.result1)  + s3 + getString(R.string.result2) );
                  
             	}
      }
